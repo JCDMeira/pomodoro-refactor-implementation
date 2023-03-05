a8f3418d7447d76729cc0fbc8cbfb8afd4a70011
Jean Meira <jean.meira10@hotmail.com>
2023-03-05 11:39:51 -0300

    chore: add and configure esLint and prettierr

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
-import Form from "./Components/Form";
-import GearButton from "./Components/GearButton";
-import classNames from "classnames";
-import "./index.css";
+import { useEffect, useState } from 'react';
+import Form from './Components/Form';
+import GearButton from './Components/GearButton';
+import classNames from 'classnames';
+import './index.css';
 
 function App() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
@@ -10,12 +10,13 @@ function App() {
 
   useEffect(() => {
     const onEscape = (e: KeyboardEvent) =>
-      e.key === "Escape" ? setIsDrawerOpen(false) : "";
+      e.key === 'Escape' ? setIsDrawerOpen(false) : '';
 
-    document.addEventListener("keydown", onEscape);
+    document.addEventListener('keydown', onEscape);
 
-    return document.removeEventListener("keydown", onEscape);
+    return document.removeEventListener('keydown', onEscape);
   }, []);
+
   return (
     <div className="text-gray-300 pt-12 md:pt-4 font-mono h-screen">
       <h1 className="text-center fs-3 mb-8">Pomodoro</h1>
@@ -34,9 +35,9 @@ function App() {
         <button
           disabled={isDrawerOpen}
           className={classNames({
-            "w-2/3 max-w-xl py-1 fs-2 lg:py-4 rounded-full uppercase": true,
-            "bg-[#602020] cursor-pointer": !isDrawerOpen,
-            "bg-[#ccc] cursor-not-allowed": isDrawerOpen,
+            'w-2/3 max-w-xl py-1 fs-2 lg:py-4 rounded-full uppercase': true,
+            'bg-[#602020] cursor-pointer': !isDrawerOpen,
+            'bg-[#ccc] cursor-not-allowed': isDrawerOpen,
           })}
           id="control-button"
         >
@@ -44,7 +45,7 @@ function App() {
         </button>
 
         <p className="whitespace-pre-wrap fs-1 font-semibold" id="notice-user">
-          {" "}
+          {' '}
         </p>
 
         <p className="cycles fs-1 font-light" id="cycles">
--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import classNames from "classnames";
-import FormInput from "../FormInput";
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import FormInput from '../FormInput';
 
 type FormProps = {
   isDrawerOpen: boolean;
@@ -21,9 +21,9 @@ const Form: React.FC<FormProps> = ({ isDrawerOpen }) => {
     <form
       id="sidebar"
       className={classNames({
-        "fixed top-0 bottom-0 right-0 min-w-fit w-2/5 pt-16 gap-y-14 flex flex-col bg-zinc-800 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border-2 border-gray-300 rounded-tl-lg rounded-bl-lg z-[1] duration-1000 ease-in-out":
+        'fixed top-0 bottom-0 right-0 min-w-fit w-2/5 pt-16 gap-y-14 flex flex-col bg-zinc-800 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border-2 border-gray-300 rounded-tl-lg rounded-bl-lg z-[1] duration-1000 ease-in-out':
           true,
-        "translate-x-full": !isDrawerOpen,
+        'translate-x-full': !isDrawerOpen,
       })}
     >
       <FormInput
